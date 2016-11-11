71e14e3715b19077beae69abbd203f7df53283a8
HEXID26_W1\Haixiang <hexid26@outlook.com>
2016-11-11 16:55:07 +0800

    减少对应的参数，一次遍历输出所有结果（全部+排序）。

--- TiebaCrawler.py
+++ TiebaCrawler.py
@@ -3,7 +3,7 @@
 #Python 3.5.2
 
 # 使用方法
-# python launcher.py --ID 4842388571 --TieziKind 0 --Date 2016-11-12 --Time 00:00 --SortedTop 0 --SortedMethod 0 --OnlySender 0 --OnlyPicsUser 0 --DownloadPics 0
+# python launcher.py --ID 4842388571 --TieziKind 0 --Date 2016-11-12 --Time 00:00 --SortedTop 0 --OnlySender 0 --OnlyPicsUser 0 --DownloadPics 0
 import sys, getopt
 import urllib.request
 import os
@@ -17,12 +17,7 @@ TieziKind = 0             # 达音科吧有点不同，设为1才能截取；正
 TieziID = 4842388571      # 帖子 ID 号
 Deadline = '2016-11-12 00:00'
 Flag_SortedTop = 0       # 打印发图总数排行前 N 个用户，0 不打印 !!!不可大于总参与用户数量，如果报错，请改小
-Flag_SortedMethod = 0     # 按照下方说明修改
-# 0. 发图数量 -> 首次回贴时间
-# 1. 发图数量 -> 贴吧等级
-# 2. 贴吧等级 -> 发图数量
 Flag_OnlySender = 0       # 1 只看楼主；0 所有楼层
-Flag_OnlyPicsUser = 0     # 1 只保存有图片的楼层；0 保存所有回复楼层
 Flag_DownloadPics = 0     # 1 下载图片；0 不下载图片
 
 
@@ -39,9 +34,7 @@ def setArgparse():
     global TieziID
     global Deadline
     global Flag_SortedTop
-    global Flag_SortedMethod
     global Flag_OnlySender
-    global Flag_OnlyPicsUser
     global Flag_DownloadPics
     parser = argparse.ArgumentParser()
     parser.add_argument("--ID", help="帖子ID号，网页地址内有")
@@ -49,28 +42,15 @@ def setArgparse():
     parser.add_argument("--Date", help="只抓取该时间以前的帖子")
     parser.add_argument("--Time", help="只抓取该时间以前的帖子")
     parser.add_argument("--SortedTop", help="0不排序，或者对前N名进行排序", type = int)
-    parser.add_argument("--SortedMethod", help="0 发图数量 -> 首次回贴时间\n1 发图数量 -> 贴吧等级\n2 贴吧等级 -> 发图数量", type = int)
     parser.add_argument("--OnlySender", help="1 只看楼主；2 全部帖子", type = int)
-    parser.add_argument("--OnlyPicsUser", help="1 只保存有图片的楼层；0 保存所有回复楼层", type = int)
     parser.add_argument("--DownloadPics", help="1 下载图片；0 不下载图片", type = int)
     args = parser.parse_args()
     TieziKind = args.TieziKind
     TieziID = args.ID
     Deadline = args.Date + ' ' + args.Time
     Flag_SortedTop = args.SortedTop
-    Flag_SortedMethod = args.SortedMethod
     Flag_OnlySender = args.OnlySender
-    Flag_OnlyPicsUser = args.OnlyPicsUser
     Flag_DownloadPics = args.DownloadPics
-    logging.debug(TieziKind)
-    logging.debug(TieziID)
-    logging.debug(Deadline)
-    logging.debug(Flag_SortedTop)
-    logging.debug(Flag_SortedMethod)
-    logging.debug(Flag_OnlySender)
-    logging.debug(Flag_OnlyPicsUser)
-    logging.debug(Flag_DownloadPics)
-    # pdb.set_trace()
     logging.debug(args)
     return
 
@@ -81,15 +61,14 @@ class PostTiezi:
         self.postSum = 0
         self.pageSum = 0
         self.title = ''
+        self.picSum = 0
 
     def add(self,con):
         # 保存信息：0 ID 1 性别 2 等级 3 楼层 4 帖子内容 5 图片内容 6 楼层数 7 图片数量
 
         # 获取信息: 性别，等级，发帖时间，楼层，ID，发帖内容
-#        logging.debug('%s' % con)
         if con[4] not in self.idList:
             self.idList.append(con[4])
-#            logging.debug('%s' % self.idList)
             tmp = list()
             tmp.append(con[4])     # 0 ID
             if con[0] == u'1':
@@ -103,14 +82,13 @@ class PostTiezi:
             tmp.append('- 第 '+str(con[3])+' 楼:\n```\n'+con[5]+'\n```\n')     # 4 帖子内容
             tmp[4] = tmp[4].replace('> \n', '')
             tmp[4] = tmp[4].rstrip(' >\n')
-#            logging.debug('-----\ncon[6] has %d items\n%s\n' % (len(con[6]), con[6]))
             if len(con[6]) != 0:
                 tmp.append(con[6])       # 5 图片内容
             else:
                 tmp.append([])
-#            logging.debug('%s'%tmp)
             tmp.append(tmp[3].count('\n')) # 6 楼层数
             tmp.append(len(con[6]))      # 7 图片数量
+            self.picSum += len(con[6])
             self.contentList.append(tmp)
         else:
             self.update(con)
@@ -118,7 +96,6 @@ class PostTiezi:
     def update(self,con):
         #更新已存在 ID 的内容
         index = self.idList.index(con[4])
-#        logging.debug(con)
         # 合并楼层索引
         self.contentList[index][3]+= str(con[3])+'楼 '+str(con[2])+'\n'
         # 合并文本回复
@@ -126,18 +103,13 @@ class PostTiezi:
         self.contentList[index][4] = self.contentList[index][4].replace('> \n', '')
         self.contentList[index][4] = self.contentList[index][4].rstrip('\n> ')
         # 插入图片信息
-#        logging.debug('-----\ncon[6] has %d items\n%s\n' % (len(con[6]), con[6]))
         if len(con[6]) != 0:
-#            logging.debug('-----\nItem at the index is:\n%s\n' % self.contentList[index])
             for item in con[6]:
                 self.contentList[index][5].append(item)
-#        logging.debug('%s' % self.contentList[index])
         # 更新统计信息
         self.contentList[index][6] += 1             # 6 楼层数
         self.contentList[index][7] += len(con[6])   # 7 图片数
-
-
-
+        self.picSum += len(con[6])
 
 class BDTB:
     def __init__(self):
@@ -178,7 +150,6 @@ class BDTB:
         pattern = re.compile(r'<h\d.*?class="core_title_txt.*?title="(.*?)"',re.S)
         # <h\d.*?class="core_title_txt.*?title="(.*?)"
         title = re.search(pattern, pagecontent)
-        logging.debug(title.group(1))
         return title.group(1)
     #获取页面回复数量和总页码
     def decodePageContentNum(self,pagecontent):
@@ -208,7 +179,6 @@ class BDTB:
 
         # 删除 title 中不能做文件名的字符
         titleName = self.title.encode('UTF-8')
-#        logging.info('titleName is %s' % titleName)
         titleName = titleName.translate(None, b'*|\/<>:"?')
         titleName = titleName.decode('UTF-8')
         logging.info('开始抓取:%s' % titleName)
@@ -258,7 +228,6 @@ class BDTB:
                     tmpList.append(con[0])
                     tmpList.append(con[1])
                     tmpList.append(con[3])
-#                logging.debug(swipList)
 
                 # 检查设置的统计截止期
                 if Deadline != '':
@@ -275,7 +244,6 @@ class BDTB:
                 else:
                     tmpList[5] = text
                 tmpList.append(pics)
-#                logging.debug(tmpList)
                 tiezi.add(tmpList)
                 cnt = int(1)
                 if Flag_DownloadPics == 1:    # 下载图片
@@ -287,119 +255,149 @@ class BDTB:
 def saveFile(listPrint):
     global NewPath
     global Flag_OnlySender
-    global Flag_OnlyPicsUser
     global tiezi
     global Deadline
+    Flag_OnlyPicsUser = 0
+    fileSaveWithPics = ['无图','有图']
+    fileSortMethodName = ['图数_回复时间','图数_贴吧等级','贴吧等级_图数']
+    for Flag_OnlyPicsUser in range(0, len(fileSaveWithPics)):
+        # 统计发图的用户数量
+        staticJoinSum = 0
+        # 用户输出编号，自用
+        cnt = int(1)
+        # 遍历所有帖子的汇总信息
+        if Flag_OnlySender==1:
+            f = open(NewPath + '/[帖子内容-楼主%s].txt' % fileSaveWithPics[Flag_OnlyPicsUser],'w',encoding='utf-8')
+        else:
+            f = open(NewPath + '/[帖子内容-全部%s].txt' % fileSaveWithPics[Flag_OnlyPicsUser],'w',encoding='utf-8')
 
-    if Flag_OnlySender==1:
-        f = open(NewPath + '/[帖子内容-楼主].txt','w',encoding='utf-8')
-    else:
-        f = open(NewPath + '/[帖子内容-全部].txt','w',encoding='utf-8')
+        f.write('# %s\n\n' % tiezi.title)
+        f.write('%s回复贴，共%s页\n统计结果于本文末尾\n' % (tiezi.postSum, tiezi.pageSum))
+        if Flag_OnlyPicsUser == 1:
+            f.write('PS：**本文只打印有照片的楼层**\n\n')
+        else:
+            f.write('PS：**本文打印所有楼层**\n\n')
+        for tmp in listPrint:
+            if Flag_OnlyPicsUser == 1 and tmp[7] == 0:
+                continue
+            f.write('----------------------------------------\n\n## ＩＤ：\t%s\n顺序编号：%d\n' % (tmp[0], cnt))
+            cnt += 1
+            f.write('性别：\t%s\n' % tmp[1])
+            f.write('等级：\t%s\n' % tmp[2])
+            f.write('\n### 楼层  贡献%d层\n' % tmp[6])
+            f.write('%s\n' % tmp[3])
+            f.write('### 文字内容\n%s\n\n' % tmp[4])
+            if tmp[7] != 0:
+                staticJoinSum += 1
+                f.write('### 图片内容 %d 张\n' % tmp[7])
+                for l in tmp[5]:
+                    f.write('![](%s)\n' % l)
+            f.write('\n')
 
-    f.write('# %s\n\n' % tiezi.title)
-    f.write('%s回复贴，共%s页\n统计结果于本文末尾\n' % (tiezi.postSum, tiezi.pageSum))
-    if Flag_OnlyPicsUser == 1:
-        f.write('PS：**本文只打印有照片的楼层**\n\n')
-    else:
-        f.write('PS：**本文打印所有楼层**\n\n')
-
-
-    staticPicSum = 0
-    staticUserSum = 0
-    staticJoinSum = 0
-    cnt = int(1)
-
-    for tmp in listPrint:
-        if Flag_OnlyPicsUser == 1 and tmp[7] == 0:
-            staticUserSum += 1
-            continue
-        f.write('----------------------------------------\n\n## ＩＤ：\t%s\n顺序编号：%d\n' % (tmp[0], cnt))
-        cnt += 1
-        f.write('性别：\t%s\n' % tmp[1])
-        f.write('等级：\t%s\n' % tmp[2])
-        f.write('\n### 楼层  贡献%d层\n' % tmp[6])
-        f.write('%s\n' % tmp[3])
-        f.write('### 文字内容\n%s\n\n' % tmp[4])
-#        logging.debug(len(tmp[5]))
-        if tmp[7] != 0:
-            staticJoinSum += 1
-            f.write('### 图片内容 %d 张\n' % tmp[7])
-            staticPicSum += tmp[7]
-            for l in tmp[5]:
-                f.write('![](%s)\n' % l)
-        f.write('\n')
-        staticUserSum += 1
-
-    # 写入统计信息
-    f.write('========================================\n# 统计结果\n\n')
-    f.write('### 回复参与人数：%d\n' % staticUserSum)
-    f.write('### 爆照参与人数：%d（楼主若发图，也算在里面。根据实际情况减除。）\n' % staticJoinSum)
-    f.write('### 收获照片总数：%d\n' % staticPicSum)
-    if Deadline == '':
-        f.write('### 统计截止日期：%s\n' % datetime.datetime.now().strftime('%Y-%m-%d %H:%M'))
-    else:
-        f.write('### 统计截止日期：%s\n' % Deadline)
-    f.write('========================================\n# 排序结果\n\n')
-
-    # 排序处理
-    global Flag_SortedMethod
-    # 0. 发图数量 -> 首次回贴时间
-    if Flag_SortedMethod == 0:
-        listPrint.sort(key=lambda x:x[7], reverse=True)
-        f.write('排名先遵循发图总数，再依据第一次回复时间\n\n')
-    # 1. 发图数量 -> 贴吧等级
-    elif Flag_SortedMethod == 1:
-        listPrint.sort(key=lambda x:(x[7],x[2]), reverse=True)
-        f.write('排名先遵循发图总数，再依据贴吧等级\n\n')
-    # 3. 贴吧等级 -> 发图数量
-    elif Flag_SortedMethod == 2:
-        listPrint.sort(key=lambda x:(x[2],x[7]), reverse=True)
-        f.write('排名先遵循贴吧等级，再依据发图数量\n\n')
-
-    f.write('|  序号  |              ID              | 贴吧等级 |        Info        |\n')
-    f.write('| :---: |             :--:             |  :---:  |        :---        |\n')
-    for cnt in range(0, len(listPrint)):
-        f.write('|  %4d  | %-30s |   %3d   |[%3d图 %3d层 %s]|\n' % (cnt+1, str(listPrint[cnt][0]), listPrint[cnt][2], listPrint[cnt][7], listPrint[cnt][6], listPrint[cnt][1]))
-    f.write('\n')
-    f.close()
-    global Flag_SortedTop
-    if Flag_SortedTop != 0:
-        f = open(NewPath + '/[Top %d - %d].txt' % (Flag_SortedTop, Flag_SortedMethod),'w',encoding='utf-8')
+        # 写入统计信息
+        f.write('========================================\n# 统计结果\n\n')
+        f.write('### 发帖参与人数：%d\n' % len(listPrint))
+        f.write('### 爆照参与人数：%d（楼主若发图，也算在里面。根据实际情况减除。）\n' % staticJoinSum)
+        f.write('### 收获照片总数：%d\n' % tiezi.picSum)
         if Deadline == '':
             f.write('### 统计截止日期：%s\n' % datetime.datetime.now().strftime('%Y-%m-%d %H:%M'))
         else:
             f.write('### 统计截止日期：%s\n' % Deadline)
+        f.write('========================================\n# 统计结果\n\n')
+        f.write('|  序号  |              ID              | 贴吧等级 |        Info        |\n')
+        f.write('| :---: |             :--:             |  :---:  |        :---        |\n')
+        for cnt in range(0, len(listPrint)):
+            f.write('|  %4d  | %-30s |   %3d   |[%3d图 %3d层 %s]|\n' % (cnt+1, str(listPrint[cnt][0]), listPrint[cnt][2], listPrint[cnt][7], listPrint[cnt][6], listPrint[cnt][1]))
+        f.write('\n')
+        f.close()
+        Flag_OnlyPicsUser += 1
+    # 全局结果保存完毕
+    # 保存所有排序结果
+    Flag_SortedMethod = 0
+    for Flag_SortedMethod in range(0,3):
+        f = open(NewPath + '/[统计结果-%s].txt' % fileSortMethodName[Flag_SortedMethod],'w',encoding='utf-8')
         if Flag_SortedMethod == 0:
-          f.write('排名先遵循发图总数，再依据第一次回复时间\n\n')
+            listPrint.sort(key=lambda x:x[7], reverse=True)
+            f.write('排名先遵循发图总数，再依据第一次回复时间\n\n')
         # 1. 发图数量 -> 贴吧等级
         elif Flag_SortedMethod == 1:
-          f.write('排名先遵循发图总数，再依据贴吧等级\n\n')
+            listPrint.sort(key=lambda x:(x[7],x[2]), reverse=True)
+            f.write('排名先遵循发图总数，再依据贴吧等级\n\n')
         # 3. 贴吧等级 -> 发图数量
         elif Flag_SortedMethod == 2:
-          f.write('排名先遵循贴吧等级，再依据发图数量\n\n')
-        for cnt in range(0, Flag_SortedTop):
-            f.write('----------------------------------------\n\n## Top：%d\nＩＤ：\t%s\n' % (cnt+1, listPrint[cnt][0]))
+            listPrint.sort(key=lambda x:(x[2],x[7]), reverse=True)
+            f.write('排名先遵循贴吧等级，再依据发图数量\n\n')
+        f.write('========================================\n# 排序结果\n\n')
+        f.write('### 发帖参与人数：%d\n' % len(listPrint))
+        f.write('### 爆照参与人数：%d（楼主若发图，也算在里面。根据实际情况减除。）\n' % staticJoinSum)
+        f.write('### 收获照片总数：%d\n' % tiezi.picSum)
+        if Deadline == '':
+            f.write('### 统计截止日期：%s\n' % datetime.datetime.now().strftime('%Y-%m-%d %H:%M'))
+        else:
+            f.write('### 统计截止日期：%s\n' % Deadline)
+        f.write('========================================\n# 统计结果\n\n')
+        f.write('|  序号  |              ID              | 贴吧等级 |        Info        |\n')
+        f.write('| :---: |             :--:             |  :---:  |        :---        |\n')
+        for cnt in range(0, len(listPrint)):
+            f.write('|  %4d  | %-30s |   %3d   |[%3d图 %3d层 %s]|\n' % (cnt+1, str(listPrint[cnt][0]), listPrint[cnt][2], listPrint[cnt][7], listPrint[cnt][6], listPrint[cnt][1]))
+        f.write('\n')
+        f.close()
+
+def saveSort(SortedTop):
+    global tiezi
+    listPrint = tiezi.contentList
+    sortMethod = ['发图总数-第一次回复时间','发图总数-贴吧等级','贴吧等级-发图数量']
+    # 如果用户数不够，修改SortedTopN
+    if SortedTop > len(tiezi.contentList):
+        SortedTop = len(tiezi.contentList)
+    # 写入文件
+    f = open(NewPath + '/[Top %d].txt' % SortedTop,'w',encoding='utf-8')
+    if Deadline == '':
+        f.write('# 统计截止日期：%s\n' % datetime.datetime.now().strftime('%Y-%m-%d %H:%M'))
+    else:
+        f.write('# 统计截止日期：%s\n' % Deadline)
+    # 写入 TOC
+    f.write('\n# 索引\n\n  * [发图总数，第一次回复时间](#%s)\n  * [发图总数，贴吧等级](#%s)\n  * [贴吧等级，发图数量](#%s)\n\n========================================\n' % (sortMethod[0], sortMethod[1], sortMethod[2]))
+    # 排序处理
+    sortMethodIndex = 0
+    for sortMethodIndex in range(0, 3):
+        # 0. 发图数量 -> 首次回贴时间
+        if sortMethodIndex == 0:
+            listPrint.sort(key=lambda x:x[7], reverse=True)
+        # 1. 发图数量 -> 贴吧等级
+        elif sortMethodIndex == 1:
+            listPrint.sort(key=lambda x:(x[7],x[2]), reverse=True)
+        # 2. 贴吧等级 -> 发图数量
+        elif sortMethodIndex == 2:
+            listPrint.sort(key=lambda x:(x[2],x[7]), reverse=True)
+        f.write('\n# [%s](#索引)\n\n' % sortMethod[sortMethodIndex])
+        # 输出所有用户的表格
+        for cnt in range(0, SortedTop):
+            f.write('----------------------------------------[返回排序标题](#%s)\n### Top：%d\nＩＤ：\t%s\n' % (sortMethod[sortMethodIndex],  cnt+1, listPrint[cnt][0]))
             f.write('性别：\t%s\n' % listPrint[cnt][1])
             f.write('等级：\t%s\n' % listPrint[cnt][2])
-            f.write('\n### 楼层  贡献%d层\n' % listPrint[cnt][6])
+            f.write('\n#### 楼层  贡献%d层\n' % listPrint[cnt][6])
             f.write('%s\n' % listPrint[cnt][3])
-            f.write('### 文字内容\n%s\n\n' % listPrint[cnt][4])
-#            logging.debug(len(listPrint[cnt][5]))
+            f.write('#### 文字内容\n%s\n\n' % listPrint[cnt][4])
             if listPrint[cnt][7] != 0:
-                f.write('### 图片内容 %d 张\n' % listPrint[cnt][7])
+                f.write('#### 图片内容 %d 张\n' % listPrint[cnt][7])
                 for l in listPrint[cnt][5]:
                     f.write('![](%s)\n' % l)
+            f.write('\n')
+        sortMethodIndex += 1
         f.write('\n')
-
-        f.close()
+    f.close()
 
 def main():
     global tiezi
+    global Flag_SortedTop
     setArgparse()
     tiezi = PostTiezi()
     bdtb = BDTB()
     bdtb.start()
     saveFile(tiezi.contentList)
+    if Flag_SortedTop > 0:
+        saveSort(Flag_SortedTop)
     logging.info('\n--%s 完成--' % tiezi.title)
     exit()
 
--- launcher.py
+++ launcher.py
@@ -13,24 +13,19 @@ logging.basicConfig(format = '%(message)s',level = logging.INFO)
 """
 在此处添加任务
 意义为
-'4842388571', '0', '2016-11-12', '00:00', '20', '0', '0', '0', '1'
+'4842388571', '0', '2016-11-12', '00:00', '20', '0', '0'
 1: 帖子ID：在地址栏可以找到
 2: 帖子种类：0 大部分帖子 1 达音科吧帖子
 3: 该时间以后的帖子不统计
 4: 该时间以后的帖子不统计
 5: Top N 排名前N位。0 表示不排序
-6: 排序方法
-      0 发图数量 -> 首次回贴时间
-      1 发图数量 -> 贴吧等级
-      2 贴吧等级 -> 发图数量
-7: 1 只看楼主；2 全部帖子
-8: 1 只保存有图片的楼层；0 保存所有回复楼层
-9: 1 下载图片；0 不下载图片
+6: 1 只看楼主；2 全部帖子
+7: 1 下载图片；0 不下载图片
 """
 
 TieziList = [
-            ['4842388571', '0', '2016-11-12', '00:00', '20', '0', '0', '1', '0'], # 国砖吧cayin爆照贴
-            # ['4836451867', '1', '2016-11-12', '00:00', '20', '0', '0', '0', '0'], # 达音科双11第二轮活动贴
+            ['4842388571', '0', '2016-11-12', '00:00', '20', '0', '0'], # 国砖吧cayin爆照贴
+            # ['4836451867', '1', '2016-11-12', '00:00', '20', '0', '0'], # 达音科双11第二轮活动贴
 ]
 
 pythonInSystem = ''
@@ -53,7 +48,7 @@ def main():
     global pythonInSystem
     checkPlatform()
     for item in TieziList:
-        processPoll.append(subprocess.Popen('%s TiebaCrawler.py --ID %s --TieziKind %s --Date %s --Time %s --SortedTop %s --SortedMethod %s --OnlySender %s --OnlyPicsUser %s --DownloadPics %s' % (pythonInSystem, item[0],item[1],item[2],item[3],item[4],item[5],item[6],item[7],item[8]), shell = True))
+        processPoll.append(subprocess.Popen('%s TiebaCrawler.py --ID %s --TieziKind %s --Date %s --Time %s --SortedTop %s --OnlySender %s --DownloadPics %s' % (pythonInSystem, item[0],item[1],item[2],item[3],item[4],item[5],item[6]), shell = True))
     while len(processPoll) != 0:
         logging.debug('工作进程数量:%d' % len(processPoll))
         for item in processPoll:
